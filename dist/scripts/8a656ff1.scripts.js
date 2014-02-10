0dbc8f3d6ced95482fd3c8c875b3774ee86338cc
nick w <nickweinberg90@gmail.com>
2014-02-10 01:22:45 -0800

    heroku go

---
+++ dist/scripts/8a656ff1.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("dailyPlannerApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("dailyPlannerApp").controller("MainCtrl",["$scope",function(a){a.events=["HTML5 Boilerplate","AngularJS","Karma"]}]);
