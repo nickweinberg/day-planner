16c0599c65cbd85e106ef3394ff786d193714c39
nick w <nickweinberg90@gmail.com>
2014-02-12 22:20:21 -0800

    Clean up code. Remove unused variables.

--- dist/scripts/8a656ff1.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("dailyPlannerApp",["ngCookies","ngResource","ngSanitize","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("dailyPlannerApp").controller("MainCtrl",["$scope",function(a){a.events=["HTML5 Boilerplate","AngularJS","Karma"]}]);
